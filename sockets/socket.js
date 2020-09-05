48d02f766df593b7794efcf246df1c7e93916d85
Aaron Alba <aalba3make@gmail.com>
2020-09-05 02:19:53 -0500

    sda

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,14 @@
 //mensja de sockets
 
+<<<<<<< HEAD
 const {io} = require('../index');
 const Band = require('../models/band');
 const Bands = require('../models/bands');
+=======
+const {io} = require('./index');
+const Band = require('./models/band');
+const Bands = require('./models/Bands');
+>>>>>>> 7ce3b6131f85b463e7a4d183f5de881c44fa10b0
 
 const bands =  new Bands();
 
